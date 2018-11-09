c85b6abdafd2d4a036c66c2cf3bedbb7023082ee
lorenzo <guiwang007@126.com>
2018-11-09 10:25:58 +0800

    first

--- src/main/java/com/soecode/lyf/web/BookController.java
+++ src/main/java/com/soecode/lyf/web/BookController.java
@@ -1,19 +1,5 @@
 package com.soecode.lyf.web;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Param;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.soecode.lyf.dto.AppointExecution;
 import com.soecode.lyf.dto.Result;
 import com.soecode.lyf.entity.Book;
@@ -21,56 +7,58 @@ import com.soecode.lyf.enums.AppointStateEnum;
 import com.soecode.lyf.exception.NoNumberException;
 import com.soecode.lyf.exception.RepeatAppointException;
 import com.soecode.lyf.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/book") // url:/模块/资源/{id}/细分 /seckill/list
+import java.util.List;
+
+@RestController("/book") // url:/模块/资源/{id}/细分 /seckill/list
 public class BookController {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	private BookService bookService;
+    @Autowired
+    private BookService bookService;
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	private String list(Model model) {
-		List<Book> list = bookService.getList();
-		model.addAttribute("list", list);
-		// list.jsp + model = ModelAndView
-		return "list";// WEB-INF/jsp/"list".jsp
-	}
+    @GetMapping(value = "/list")
+    private Object list(Model model) {
+        List<Book> list = bookService.getList();
+        return new Result<>(true, list);
+    }
 
-	@RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
-	private String detail(@PathVariable("bookId") Long bookId, Model model) {
-		if (bookId == null) {
-			return "redirect:/book/list";
-		}
-		Book book = bookService.getById(bookId);
-		if (book == null) {
-			return "forward:/book/list";
-		}
-		model.addAttribute("book", book);
-		return "detail";
-	}
+    @RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
+    private Object detail(@PathVariable("bookId") Long bookId) {
+        if (bookId == null) {
+            return new Result<>(false, "bookId不能为空");
+        }
+        Book book = bookService.getById(bookId);
+        if (book == null) {
+            return new Result<>(false, "book不存在");
+        }
+        return new Result<>(true, book);
+    }
 
-	// ajax json
-	@RequestMapping(value = "/{bookId}/appoint", method = RequestMethod.POST, produces = {
-			"application/json; charset=utf-8" })
-	@ResponseBody
-	private Result<AppointExecution> appoint(@PathVariable("bookId") Long bookId, @RequestParam("studentId") Long studentId) {
-		if (studentId == null || studentId.equals("")) {
-			return new Result<>(false, "学号不能为空");
-		}
-		AppointExecution execution = null;
-		try {
-			execution = bookService.appoint(bookId, studentId);
-		} catch (NoNumberException e1) {
-			execution = new AppointExecution(bookId, AppointStateEnum.NO_NUMBER);
-		} catch (RepeatAppointException e2) {
-			execution = new AppointExecution(bookId, AppointStateEnum.REPEAT_APPOINT);
-		} catch (Exception e) {
-			execution = new AppointExecution(bookId, AppointStateEnum.INNER_ERROR);
-		}
-		return new Result<AppointExecution>(true, execution);
-	}
+    // ajax json
+    @RequestMapping(value = "/{bookId}/appoint", method = RequestMethod.POST, produces = {
+            "application/json; charset=utf-8"})
+    private Result<AppointExecution> appoint(@PathVariable("bookId") Long bookId, @RequestParam("studentId") Long studentId) {
+        if (studentId == null || studentId.equals("")) {
+            return new Result<>(false, "学号不能为空");
+        }
+        AppointExecution execution = null;
+        try {
+            execution = bookService.appoint(bookId, studentId);
+        } catch (NoNumberException e1) {
+            execution = new AppointExecution(bookId, AppointStateEnum.NO_NUMBER);
+        } catch (RepeatAppointException e2) {
+            execution = new AppointExecution(bookId, AppointStateEnum.REPEAT_APPOINT);
+        } catch (Exception e) {
+            execution = new AppointExecution(bookId, AppointStateEnum.INNER_ERROR);
+        }
+        return new Result<AppointExecution>(true, execution);
+    }
 
 }
